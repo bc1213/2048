e4953f561fa4e334d954808a87c2935e35f31dd9
Dev <bharathr636@gmail.com>
2021-09-07 16:33:21 +0530

    DEV: Handled action for swipe [left,right, bottom,top]

--- app/src/main/java/com/abln/game2048/GameDashboardActivity.kt
+++ app/src/main/java/com/abln/game2048/GameDashboardActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_gamedashboard.*
 import java.util.*
 import kotlin.collections.ArrayList
@@ -13,35 +14,34 @@ import kotlin.collections.HashMap
 
 class GameDashboardActivity : AppCompatActivity() {
 
-
     val colorMapper: HashMap<Int, String> = HashMap();
     var filledCell = ArrayList<Int>()
+    var currentState = Array(4) {Array(4) {0} }
+    var updatedState = Array(4) {Array(4) {0} }
 
     enum class SwipeDirection {
-        top,
-        bottom,
-        left,
-        right
+        Top,
+        Bottom,
+        Left,
+        Right
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gamedashboard)
 
-        colorMapper[2] = "#eee4da"// R.color.cellLayoutBgColor_2.toString()
-        colorMapper[4] = "#ede0c7"//R.color.cellLayoutBgColor_4.toString()
-        colorMapper[8] = "#f2b178"//R.color.cellLayoutBgColor_8.toString()
-        colorMapper[16] = "#f59563"//R.color.cellLayoutBgColor_16.toString()
-        colorMapper[32] = "#f67c5f"// R.color.cellLayoutBgColor_32.toString()
-        colorMapper[64] = "#f65d3b"// R.color.cellLayoutBgColor_64.toString()
-        colorMapper[128] = "#E4CF8B"//R.color.cellLayoutBgColor_128.toString()
-        colorMapper[256] = "#edcc61"//R.color.cellLayoutBgColor_256.toString()
-        colorMapper[512] = "#EEC43A"//R.color.cellLayoutBgColor_512.toString()
-        colorMapper[1024] = "#97ED61"//R.color.cellLayoutBgColor_1024.toString()
-        colorMapper[2048] = "#B02DF2"//R.color.cellLayoutBgColor_2048.toString()
-
-        colorMapper[2048]?.let { Log.d("PrintMsg", it) }
-        print(colorMapper[2048])
+        colorMapper[0] = "#CDC1B4"
+        colorMapper[2] = "#eee4da"
+        colorMapper[4] = "#ede0c7"
+        colorMapper[8] = "#f2b178"
+        colorMapper[16] = "#f59563"
+        colorMapper[32] = "#f67c5f"
+        colorMapper[64] = "#f65d3b"
+        colorMapper[128] = "#E4CF8B"
+        colorMapper[256] = "#edcc61"
+        colorMapper[512] = "#EEC43A"
+        colorMapper[1024] = "#97ED61"
+        colorMapper[2048] = "#B02DF2"
 
         addSwipeGesture(swipeArea)
         playFirstMove()
@@ -54,37 +54,325 @@ class GameDashboardActivity : AppCompatActivity() {
 
             override fun onSwipeTop() {
                 super.onSwipeTop()
-                Log.d("GameDashboard","top"+SwipeDirection.top);
+                //call swipe default function
+                swipeAction(SwipeDirection.Top)
             }
 
             override fun onSwipeBottom() {
                 super.onSwipeBottom()
-                Log.d("GameDashboard","bottom"+SwipeDirection.bottom);
+                swipeAction(SwipeDirection.Bottom)
             }
 
             override fun onSwipeLeft() {
                 super.onSwipeLeft()
-                Log.d("GameDashboard","left"+SwipeDirection.left);
+                swipeAction(SwipeDirection.Left)
             }
 
             override fun onSwipeRight() {
                 super.onSwipeRight()
-                Log.d("GameDashboard","right"+SwipeDirection.right);
-
+                swipeAction(SwipeDirection.Right)
             }
         })
     }
 
-    fun removeSwipeGesture(view: View) {
-        view.isClickable = false
+    fun swipeAction(action: SwipeDirection) {
+        //save the current state into two dimensional array
+        saveCurrentStateTo2DArray()
+
+        when(action) {
+            SwipeDirection.Top -> {
+                performActionTop()
+            }
+
+            SwipeDirection.Bottom -> {
+                performActionBottom()
+            }
+
+            SwipeDirection.Left -> {
+                performActionLeft()
+            }
+
+            SwipeDirection.Right -> {
+                performActionRight()
+            }
+        }
+
+        loadFromUpdatedStateToUI()
+        updateUIaccordingToNumber()
+        //Call value in empty space
+        generateNewRandomNumberAfterSwipeAction()
     }
 
-    fun launchNewGame() {
-        //reset all cells
-        restartGame()
+    fun updateUIaccordingToNumber() {
+        //First row
+        tv1.setBackgroundColor(Color.parseColor(colorMapper[updatedState[0][0]]))
+        tv2.setBackgroundColor(Color.parseColor(colorMapper[updatedState[0][1]]))
+        tv3.setBackgroundColor(Color.parseColor(colorMapper[updatedState[0][2]]))
+        tv4.setBackgroundColor(Color.parseColor(colorMapper[updatedState[0][3]]))
 
-        // use random generator and place 2's and 4's inside the 16 places
+        //Second row
+        tv5.setBackgroundColor(Color.parseColor(colorMapper[updatedState[1][0]]))
+        tv6.setBackgroundColor(Color.parseColor(colorMapper[updatedState[1][1]]))
+        tv7.setBackgroundColor(Color.parseColor(colorMapper[updatedState[1][2]]))
+        tv8.setBackgroundColor(Color.parseColor(colorMapper[updatedState[1][3]]))
 
+        //Third row
+        tv9.setBackgroundColor(Color.parseColor(colorMapper[updatedState[2][0]]))
+        tv10.setBackgroundColor(Color.parseColor(colorMapper[updatedState[2][1]]))
+        tv11.setBackgroundColor(Color.parseColor(colorMapper[updatedState[2][2]]))
+        tv12.setBackgroundColor(Color.parseColor(colorMapper[updatedState[2][3]]))
+
+        //Fourth row
+        tv13.setBackgroundColor(Color.parseColor(colorMapper[updatedState[3][0]]))
+        tv14.setBackgroundColor(Color.parseColor(colorMapper[updatedState[3][1]]))
+        tv15.setBackgroundColor(Color.parseColor(colorMapper[updatedState[3][2]]))
+        tv16.setBackgroundColor(Color.parseColor(colorMapper[updatedState[3][3]]))
+    }
+
+    fun performActionTop() {
+//        copy current state to final state
+        updatedState = currentState
+
+        //check if first two rows can be added
+
+        for(row:Int in 1 until currentState.size) {
+            for(col: Int in currentState[row].indices) {
+                if(currentState[row-1][col] == currentState[row][col]) {
+                    //add these tow number together and replace the currentState to 0
+                    currentState[row-1][col] += currentState[row][col]
+                    currentState[row][col] = 0
+                }
+            }
+        }
+
+        //move the position from bottom to top
+        for(row in 3 downTo 1) {
+            for(col: Int in currentState[row].indices) {
+
+                if(currentState[row-1][col] == 0) {
+                    //add these tow number together and replace the currentState to 0
+                    currentState[row-1][col] = currentState[row][col]
+                    currentState[row][col] = 0
+                }
+            }
+        }
+
+        updateFreeCells()
+        updatedState = currentState
+    }
+
+    fun performActionBottom() {
+        //        copy current state to final state
+        updatedState = currentState
+
+        //check if first two rows can be added
+
+        for(row:Int in currentState.size-1 downTo 1) {
+            for(col: Int in currentState[row].indices) {
+                if(currentState[row-1][col] == currentState[row][col]) {
+                    //add these tow number together and replace the currentState to 0
+                    currentState[row][col] += currentState[row-1][col]
+                    currentState[row-1][col] = 0
+                }
+            }
+        }
+
+        //move the position from bottom to top
+        for(row in 1 until currentState.size) {
+            for(col: Int in currentState[row].indices) {
+
+                if(currentState[row][col] == 0) {
+                    //add these tow number together and replace the currentState to 0
+                    currentState[row][col] = currentState[row-1][col]
+                    currentState[row-1][col] = 0
+                }
+            }
+        }
+
+        updateFreeCells()
+        updatedState = currentState
+
+    }
+
+    fun performActionLeft() {
+        //        copy current state to final state
+        updatedState = currentState
+
+        //check if first two rows can be added
+
+        for(col:Int in 1 until currentState.size) {
+            for(row: Int in currentState[col].indices) {
+                if(currentState[row][col-1] == currentState[row][col]) {
+                    //add these tow number together and replace the currentState to 0
+                    currentState[row][col-1] += currentState[row][col]
+                    currentState[row][col] = 0
+                }
+            }
+        }
+
+        //move the position from bottom to top
+        for(col in 3 downTo 1) {
+            for(row: Int in currentState[col].indices) {
+
+                if(currentState[row][col-1] == 0) {
+                    //add these tow number together and replace the currentState to 0
+                    currentState[row][col-1] = currentState[row][col]
+                    currentState[row][col] = 0
+                }
+            }
+        }
+
+        updateFreeCells()
+        updatedState = currentState
+    }
+
+    fun performActionRight() {
+        //        copy current state to final state
+        updatedState = currentState
+
+        //check if first two rows can be added
+
+        for(col:Int in currentState.size-1 downTo 1) {
+            for(row: Int in currentState[col].indices) {
+                if(currentState[row][col-1] == currentState[row][col]) {
+                    //add these tow number together and replace the currentState to 0
+                    currentState[row][col] += currentState[row][col-1]
+                    currentState[row][col-1] = 0
+                }
+            }
+        }
+
+        //move the position from bottom to top
+        for(col in 1 until currentState.size) {
+            for(row: Int in currentState[col].indices) {
+
+                if(currentState[row][col] == 0) {
+                    //add these tow number together and replace the currentState to 0
+                    currentState[row][col] = currentState[row][col-1]
+                    currentState[row][col-1] = 0
+                }
+            }
+        }
+
+        updateFreeCells()
+        updatedState = currentState
+
+    }
+
+    fun updateFreeCells() {
+        filledCell.clear()
+
+        for(row: Int in 0 until 4) {
+            if(currentState[0][row] != 0) {
+                filledCell.add(row + 1)
+            }
+
+            if(currentState[1][row] != 0) {
+                filledCell.add(row + 5)
+            }
+
+            if(currentState[2][row] != 0) {
+                filledCell.add(row + 9)
+            }
+
+            if(currentState[3][row] != 0) {
+                filledCell.add(row + 13)
+            }
+        }
+        Log.d("TAG", "filledCellCount"+filledCell.size)
+    }
+
+    fun saveCurrentStateTo2DArray() {
+        //FirstRow
+        currentState[0][0] = tv1.text.toString().toIntWithNullHandle();
+        currentState[0][1] = tv2.text.toString().toIntWithNullHandle();
+        currentState[0][2] = tv3.text.toString().toIntWithNullHandle();
+        currentState[0][3] = tv4.text.toString().toIntWithNullHandle();
+
+        //SecondRow
+        currentState[1][0] = tv5.text.toString().toIntWithNullHandle();
+        currentState[1][1] = tv6.text.toString().toIntWithNullHandle();
+        currentState[1][2] = tv7.text.toString().toIntWithNullHandle();
+        currentState[1][3] = tv8.text.toString().toIntWithNullHandle();
+
+        //ThirdRow
+        currentState[2][0] = tv9.text.toString().toIntWithNullHandle();
+        currentState[2][1] = tv10.text.toString().toIntWithNullHandle();
+        currentState[2][2] = tv11.text.toString().toIntWithNullHandle();
+        currentState[2][3] = tv12.text.toString().toIntWithNullHandle();
+
+        //FourthRow
+        currentState[3][0] = tv13.text.toString().toIntWithNullHandle();
+        currentState[3][1] = tv14.text.toString().toIntWithNullHandle();
+        currentState[3][2] = tv15.text.toString().toIntWithNullHandle();
+        currentState[3][3] = tv16.text.toString().toIntWithNullHandle();
+    }
+
+    fun loadFromUpdatedStateToUI() {
+        //FirstRow
+        tv1.text = updatedState[0][0].toStringWithZeroHandle()
+        tv2.text = updatedState[0][1].toStringWithZeroHandle()
+        tv3.text = updatedState[0][2].toStringWithZeroHandle()
+        tv4.text = updatedState[0][3].toStringWithZeroHandle()
+
+        //SecondRow
+        tv5.text = updatedState[1][0].toStringWithZeroHandle()
+        tv6.text = updatedState[1][1].toStringWithZeroHandle()
+        tv7.text = updatedState[1][2].toStringWithZeroHandle()
+        tv8.text = updatedState[1][3].toStringWithZeroHandle()
+
+        //ThirdRow
+        tv9.text = updatedState[2][0].toStringWithZeroHandle()
+        tv10.text = updatedState[2][1].toStringWithZeroHandle()
+        tv11.text = updatedState[2][2].toStringWithZeroHandle()
+        tv12.text = updatedState[2][3].toStringWithZeroHandle()
+
+        //FourthRow
+        tv13.text = updatedState[3][0].toStringWithZeroHandle()
+        tv14.text = updatedState[3][1].toStringWithZeroHandle()
+        tv15.text = updatedState[3][2].toStringWithZeroHandle()
+        tv16.text = updatedState[3][3].toStringWithZeroHandle()
+    }
+
+    fun generateNewRandomNumberAfterSwipeAction() {
+        var emptyCell = ArrayList<Int>()
+
+        for (cellId in 1..16) {
+            if((!filledCell.contains(cellId))) {
+                emptyCell.add(cellId)
+            }
+        }
+
+        if(emptyCell.size == 0) {
+            Toast.makeText(this, "Game over! Start new game", Toast.LENGTH_SHORT).show()
+            return;
+        }
+        Log.d("TAG","Emptycellsize"+emptyCell.size)
+        val r = Random()
+        val randIndex = r.nextInt(emptyCell.size)
+        val cellId = emptyCell[randIndex]
+
+        var tvSelected: TextView?
+        tvSelected = when(cellId) {
+            1-> tv1
+            2-> tv2
+            3-> tv3
+            4-> tv4
+            5-> tv5
+            6-> tv6
+            7-> tv7
+            8-> tv8
+            9-> tv9
+            10-> tv10
+            11-> tv11
+            12-> tv12
+            13-> tv13
+            14-> tv14
+            15-> tv15
+            16-> tv16
+            else -> { tv1}
+        }
+        playGame(cellId, tvSelected, generateCellRandomNumber())
     }
 
     fun restartGame() {
@@ -197,4 +485,18 @@ class GameDashboardActivity : AppCompatActivity() {
     fun launchNewGame(view: android.view.View) {
         restartGame()
     }
+}
+
+fun String.toIntWithNullHandle(): Int {
+    if(this.contentEquals("")) {
+        return 0
+    }
+    return this.toInt()
+}
+
+fun Int.toStringWithZeroHandle(): String {
+    if(this == 0) {
+        return ""
+    }
+    return this.toString()
 }
